8a19652a2338639f990170cc96ebf52bdb3dd457
Firdavs Xalikov <f.x@MacBook-Air-Firdavs.local>
2024-09-09 18:06:21 +0500

    new

--- src/Home.jsx
+++ src/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
         console.log(data.ism);
         console.log(2);
         
-        e.preventDefault()
+        data.preventDefault()
         // window.location.reload();
         axios
             .post(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
